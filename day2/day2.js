3daba484d6f34c62c40a0b2b0cee18f9c5290555
Li Peihua <1150930095@qq.com>
2019-06-12 10:03:14 +0800

    Add files via upload

--- day2/day2.js
+++ day2/day2.js
@@ -2,31 +2,32 @@
 // 如何可视宽度 少于 800 不显示sideBar
 var closeBtn = document.getElementsByClassName("closeBtn")[0];
 var sideBar = document.getElementsByClassName("sideBar")[0];
+var wrap = document.getElementsByClassName("wrap")[0];
 var key = true;
 
 // 如可视宽度少于 800 没点击关闭 sidebar 不显示sideBar 
 // 如监控可视区域大于800 没点击关闭 显示 sidebar
 
-
-function closeBar(){
+function allow_closeBar(){
 	if(key){
-		closeBtn.addEventListener('click',function(){
-		   sideBar.style.display = "none";
-		   key = false;
-		})
+		sideBar.style.visibility = "visible";
+		closeBtn.onclick = function(){
+			sideBar.className += " close";
+			key = false;
+		}
 	}
 }
 
 function widthMonitor(){
 	var userWidth = document.body.offsetWidth;
 	if(userWidth > 800 && key == true){
-        sideBar.style.display = "block";
-        closeBar();
+        sideBar.style.visibility = "visible";
+        allow_closeBar();
 	}else if(userWidth < 800 ){
-		sideBar.style.display = "none";
+		sideBar.style.visibility = "hidden";
 	}else{
-        closeBar();
+        allow_closeBar();
 	}
 }
 
- setInterval(widthMonitor,10)
+widthMonitor();
