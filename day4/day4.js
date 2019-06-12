50175f632f5faa88f726a2297c7246e901552a30
Li Peihua <1150930095@qq.com>
2019-06-12 23:44:45 +0800

    Add files via upload

--- day4/day4.js
+++ day4/day4.js
@@ -13,6 +13,9 @@ var styleName,
 var styleOptions = document.getElementsByClassName('styleOption');
 var inputValue = document.getElementsByClassName('inputValue');
 var restore = document.getElementsByClassName('restore')[0];
+var show_index = 0;
+var timer;
+var show_key = true;
 
 for(var i = 0;i<images.length;i++){
 	let k = i;
@@ -24,8 +27,10 @@ for(var i = 0;i<images.length;i++){
 function showPic(whichpic){
 	var source = whichpic.getAttribute("src");
 	var title = whichpic.getAttribute("title");
+	var url = "url(" +   source   + ")";
 	container.style.backgroundImage = "url(" +   source   + ")";
 	placeholder.innerHTML = title;
+	console.log(url);
 }
 // 自由选择需要修改的style和style value，点击提交后，修改显示在container的图片
 function changeStyle(){
@@ -40,10 +45,33 @@ function changeStyle(){
 		})
 	}
     restore.addEventListener('click',function(){
-    	var bgImg = container.style.backgroundImage;
-    	container.style = " ";
-    	container.style.backgroundImage = bgImg;
-    	console.log(bgImg);
+    	if(show_key){
+	        var bgImg = container.style.backgroundImage;
+	    	container.style = " ";
+	    	container.style.backgroundImage = bgImg;
+	    	clearInterval(timer);
+	    	this.innerHTML = "开始轮播，修改样式";
+	    	console.log(show_key);
+	    	show_key = false;
+    	}else{
+    		clearInterval(timer);
+    		timer = setInterval(relayShow,2000);
+    		this.innerHTML = "停止轮播，清除样式";
+    	}
     })
 }
 changeStyle();
+
+timer = setInterval(relayShow,2000);
+
+function relayShow(){
+     if(show_index >= 0 && show_index < images.length){
+     	let current_image = images[show_index];
+        container.style.backgroundImage = "url(" + current_image.getAttribute("src")  + ")";
+        placeholder.innerHTML = current_image.getAttribute("title");
+        show_index++;
+        console.log(show_index);
+     }else{
+     	show_index = 0;
+     }
+}
