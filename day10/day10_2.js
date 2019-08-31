7e81075440961e13020766157a83d615d4e6fb9c
Li Peihua <1150930095@qq.com>
2019-08-31 21:29:32 +0800

    Add files via upload

---
+++ day10/day10_2.js
@@ -0,0 +1,39 @@
+
+// 如何可视宽度 少于 800 不显示sideBar
+var closeBtn = document.getElementsByClassName("closeBtn")[0];
+var sideBar = document.getElementsByClassName("sideBar")[0];
+var wrap = document.getElementsByClassName("wrap")[0];
+var key = true;
+var more = document.getElementsByClassName("more")[0];
+var text = document.getElementsByClassName("text")[0];
+
+more.addEventListener('click',function(){
+	text.style.visibility = "visible";
+})
+
+// 如可视宽度少于 800 没点击关闭 sidebar 不显示sideBar 
+// 如监控可视区域大于800 没点击关闭 显示 sidebar
+
+function allow_closeBar(){
+	if(key){
+		sideBar.style.visibility = "visible";
+		closeBtn.onclick = function(){
+			sideBar.className += " close";
+			key = false;
+		}
+	}
+}
+
+function widthMonitor(){
+	var userWidth = document.body.offsetWidth;
+	if(userWidth > 800 && key == true){
+        sideBar.style.visibility = "visible";
+        allow_closeBar();
+	}else if(userWidth < 800 ){
+		sideBar.style.visibility = "hidden";
+	}else{
+        allow_closeBar();
+	}
+}
+
+widthMonitor();
